27cc6e3a0d8b096ef753dd3763bd4be4976cb423
Jim Merioles <jimwisleymerioles@gmail.com>
2016-07-31 00:24:00 +0800

    Profile page.

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,322 +1,155 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
+@extends('layouts.main')
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css/mainstyle.css">
-    <link rel="shortcut icon" href="img/furpawlogo.png">
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="css/thumbnail.css">
-    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
-    <link href="css/hover-min.css" rel="stylesheet" media="all">
-    <link rel="stylesheet" href="css/animate.css">
-    <title>FurAndPaws-Take a tour for your pets</title>
+@section('title', 'Profile')
 
+@push('head-css')
+<link rel="stylesheet" type="text/css" href="css/thumbnail.css">
+@endpush
 
-</head>
-
-<body>
-<!-- Modals	 -->
-
-<!--Login Modal -->
-<div id="login-modal">
-
-    <div class="login-content">
-        <div class="login-header">
-            <div id="login-left" class="login-header-left">
-
-                <h4><i class="fa fa-key"></i> LOG IN</h4>
+@section('content')
+    <div class="header-container">
+        <div class="header-content">
+            <div class="header-left">
+                <h1>John Louise Berdida</h1>
             </div>
-            <div id="login-right" class="login-header-right unselect">
-
-                <h4><i class="fa fa-users"></i> CREATE AN ACCOUNT</h4>
+            <div class="header-right">
+                <ol class="breadcrumb">
+                    <li><a href="index.html">Home</a></li>
+                    <li class="active">Profile</li>
+                </ol>
             </div>
-
-            <div class="clearfix"></div>
-
-            <div class="login-main-content">
-
-                <div class="content-signin"></div>
-                <div class="clearfix"></div>
-                <div class="content-signup"></div>
-
-            </div>
-
-
-        </div><!-- end of login-header -->
-    </div>
-</div>
-
-</div>
-
-
-<a href="#" class="scroll_to_top hvr-grow" id="scroll"><span class="fa fa-angle-up fa-2x"></span></a>
-
-<div class="menu_user_wrap">
-
-    <div class="content_wrap clearfix">
-
-        <div class="menu_user_area menu_user_right menu_user_contact_area">
-            <ul>
-                <li ><i class="fa fa-user fa-lg"></i><a href="#" id="loginmodal"> Login / Sign-up</a></li>
-
-
-            </ul>
         </div>
+    </div><!-- end of header container -->
 
-        <div class="menu_user_area menu_user_contact_area menu_user_left">Welcome to Fur And Paws!
-        </div>
-    </div>
+    <div class="content-wrap clearfix">
+        <div class="section-content clearfix">
+            <div class="content-left">
+                <article>
+                    <section>
+                        <div id="buddypress">
+                            <div id="item-header">
+                                <div id="cover-image-container">
+                                    <a href="#" id="header-cover-image"><img src="img/thumbnail/timeline.jpg"></a>
 
+                                    <div id="item-header-cover-image">
+                                        <div id="item-header-avatar">
+                                            <a href="#"><img src="img/thumbnail/john.jpg"
+                                                             class="avatar avatar-150 photo"/></a>
+                                        </div>
+                                        <div id="item-header-content">
+                                            <h2 class="user-nicename">John Louise Berdida</h2>
 
+                                            <div id="item-buttons"></div>
+                                            <span class="activity">active 1 day, 20 hours ago</span>
 
-
-</div><!-- End of small header -->
-
-
-
-
-
-
-<div class="nav_container clearfix">
-
-    <div class="nav_main">
-        <div class="nav_left">
-            <a href="index.html"><img src="img/furpawlogo.png"></a>
-        </div>
-
-        <div class="nav_right">
-            <ul>
-                <li>
-                    <a href="index.html" class="active">Home</a>
-
-                    <ul class="sub_nav" id="sub1">
-                        <li class="li-style"><a href="petstore.blade.php">Pet Store</a></li>
-                        <li class="li-style"><a href="#">Pet Walking</a></li>
-                        <li class="li-style"><a href="#">Adoption</a></li>
-                        <li class="li-style"><a href="#">Club</a></li>
-                    </ul>
-                </li>
-                <li>
-
-                    <a href="#">Features</a>
-                    <ul class="sub_nav" id="sub2">
-                        <li class="li-style"><a href="#">Events</a></li>
-                        <li class="li-style"><a href="#">Our Services</a></li>
-                        <li class="li-style"><a href="#">Contact</a></li>
-                        <li class="li-style"><a href="#">Our Team</a></li>
-                    </ul>
-                </li>
-                <li>
-
-                    <a href="#">Gallery</a>
-                    <ul class="sub_nav" id="sub3">
-                        <li class="li-style"><a href="#">Pets Adoption</a></li>
-                        <li class="li-style"><a href="#">Pets Sale</a></li>
-                    </ul>
-                </li>
-                <li><a href="post-sale.html">Shop</a></li>
-                <li><a href="adopt.html"><button class="button green hvr-pulse"><i class="fa fa-check-circle" ></i> ADOPT</button></a></li>
-                <li><button class="button red hvr-pulse"><span class="glyphicon glyphicon-heart"></span> FIND A BREEDER</button></li>
-
-            </ul>
-        </div>
-
-    </div>
-</div><!-- End of Nav -->
-
-
-<div class="header-container">
-
-    <div class="header-content">
-
-        <div class="header-left">
-            <h1>John Louise Berdida</h1>
-        </div>
-
-        <div class="header-right">
-            <ol class="breadcrumb">
-                <li><a href="index.html">Home</a></li>
-                <li class="active">Profile</li>
-            </ol>
-        </div>
-
-    </div>
-
-
-</div><!-- end of header container -->
-
-
-<div class="content-wrap clearfix">
-
-    <div class="section-content clearfix">
-
-        <div class="content-left">
-
-        <article>
-            <section>
-                <div id="buddypress">
-                    <div id="item-header">
-                        <div id="cover-image-container">
-                            <a href="#" id="header-cover-image"><img src="img/thumbnail/timeline.jpg"></a>
-                            <div id="item-header-cover-image">
-                                <div id="item-header-avatar">
-                                    <a href="#" ><img src="img/thumbnail/john.jpg" class="avatar avatar-150 photo"/></a>
-                                </div>
-
-                                <div id="item-header-content">
-                                    <h2 class="user-nicename">John Louise Berdida</h2>
-                                    <div id="item-buttons"></div>
-                                    <span class="activity">active 1 day, 20 hours ago</span>
-                                    <div id="item-meta">
-                                        <div id="latest-update"></div>
+                                            <div id="item-meta">
+                                                <div id="latest-update"></div>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-
-
-                        </div>
-                    </div> <!--item-header-->
-
-                    <div id="item-nav">
-                        <div class="item-list-tabs no-ajax" id="objectnav" role="navigation">
-                            <ul>
-                                <li id="activity-personal-li" class="current select">
-                                    <a href="#" id="user-activity">Activity</a>
-                                </li>
-                                <li id="xprofile-personal-li" class="current">
-                                    <a id="user-xprofile" href="#">Profile</a>
-                                </li>
-                                <li id="friends-personal-li" class="current">
-                                    <a id="user-friends" href="#">Friends <span class="count">1</span></a>
-                                </li>
-                                <li id="groups-personal-li" class="current">
-                                    <a id="user-groups" href="#">Groups <span class="count">4</span></a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div><!--item-nav-->
-
-                    <div id="item-body">
-                            <div><a href="#">Personal</a></div>
-                            <div><a href="#">Mentions</a></div>
-                            <div><a href="#">Favorites</a></div>
-                            <div><a href="#">Friends</a></div>
-                            <div style="width:400px"><span style="padding-right: 10px;">Show: </span>
-                                <select>
-                                    <option>--Everything--</option>
-                                    <option>Posts</option>
-                                    <option>Friendships</option>
-                                    <option>Comments</option>
-                                    <option>Updates</option>
-                                </select>
+                            <!--item-header-->
+                            <div id="item-nav">
+                                <div class="item-list-tabs no-ajax" id="objectnav" role="navigation">
+                                    <ul>
+                                        <li id="activity-personal-li" class="current select">
+                                            <a href="#" id="user-activity">Activity</a>
+                                        </li>
+                                        <li id="xprofile-personal-li" class="current">
+                                            <a id="user-xprofile" href="#">Profile</a>
+                                        </li>
+                                        <li id="friends-personal-li" class="current">
+                                            <a id="user-friends" href="#">Friends <span class="count">1</span></a>
+                                        </li>
+                                        <li id="groups-personal-li" class="current">
+                                            <a id="user-groups" href="#">Groups <span class="count">4</span></a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
-                    </div>
-                    <div class="clearfix"></div>
-
-
-                </div>
-            </section>
-            <hr>
-            <br>
-            <ol class="commentlist">
-                <li class="profile-border">
-                    <div class="comment-16">
-                        <img src="img/thumbnail/john.jpg" class="avatar">
-
-                        <div class="comment-text">
-
-
-                            <p class="meta">
-                                <strong>John Louise Berdida</strong>
-                                -
-                                <time>Posted: 1 Hour ago:</time>
-                            </p>
-                            <div class="description">
-                                Batiag nawng ana imong pets oie . .Bdw interesting ko nga mopalit ana niya kay ipakaon nako sa akong binuhi nga dragon
-                                mo ask lang ta ko kung dili bana matakdan sa bun.i akong dragon nig kaon ana niya kay gi bun.i raba na imong pets or
-                                tambalan lang sa nako na ang lubot ana imong pets usa akong ipakaon ?
+                            <!--item-nav-->
+                            <div id="item-body">
+                                <div><a href="#">Personal</a></div>
+                                <div><a href="#">Mentions</a></div>
+                                <div><a href="#">Favorites</a></div>
+                                <div><a href="#">Friends</a></div>
+                                <div style="width:400px"><span style="padding-right: 10px;">Show: </span>
+                                    <select>
+                                        <option>--Everything--</option>
+                                        <option>Posts</option>
+                                        <option>Friendships</option>
+                                        <option>Comments</option>
+                                        <option>Updates</option>
+                                    </select>
+                                </div>
                             </div>
+                            <div class="clearfix"></div>
                         </div>
-                    </div>
+                    </section>
+                    <hr>
+                    <br>
+                    <ol class="commentlist">
+                        <li class="profile-border">
+                            <div class="comment-16">
+                                <img src="img/thumbnail/john.jpg" class="avatar">
 
-                </li> <!--end of first comment-->
-             </ol>
+                                <div class="comment-text">
+                                    <p class="meta">
+                                        <strong>John Louise Berdida</strong>
+                                        -
+                                        <time>Posted: 1 Hour ago:</time>
+                                    </p>
+                                    <div class="description">
+                                        Batiag nawng ana imong pets oie . .Bdw interesting ko nga mopalit ana niya kay
+                                        ipakaon nako sa akong binuhi nga dragon
+                                        mo ask lang ta ko kung dili bana matakdan sa bun.i akong dragon nig kaon ana
+                                        niya kay gi bun.i raba na imong pets or
+                                        tambalan lang sa nako na ang lubot ana imong pets usa akong ipakaon ?
+                                    </div>
+                                </div>
+                            </div>
+                        </li>
+                        <!--end of first comment-->
+                    </ol>
+                </article>
+            </div>
+            <div class="widget-right">
+                <aside class="widget">
+                    <h5 class="widget_title">
+                        Search Members
+                    </h5>
 
-        </article>
-
-
-
+                    <form class="standard-form">
+                        <div>
+                            <label>Name</label>
+                            <input type="text"/>
+                        </div>
+                        <div>
+                            <label>Age</label>
+                            <input type="text"/>
+                        </div>
+                        <div>
+                            <label>Pet Breed</label>
+                            <select>
+                                <option>--Select Breed--</option>
+                                <option>German Shepherd</option>
+                                <option>Askal</option>
+                                <option>Pussy</option>
+                                <option>lang mananap</option>
+                            </select>
+                        </div>
+                        <div class="submit">
+                            <button class="button-search hvr-pulse">SEARCH</button>
+                        </div>
+                    </form>
+                </aside>
+            </div>
         </div>
+    </div> <!--content-wrap-->
+@endsection
 
-
-        <div class="widget-right">
-            <aside class="widget">
-                <h5 class="widget_title">
-                    Search Members
-                </h5>
-
-                <form class="standard-form">
-                    <div>
-                        <label>Name</label>
-                        <input type="text"/>
-                    </div>
-                    <div>
-                        <label>Age</label>
-                        <input type="text"/>
-                    </div>
-                    <div>
-                        <label>Pet Breed</label>
-                        <select>
-                            <option>--Select Breed--</option>
-                            <option>German Shepherd</option>
-                            <option>Askal</option>
-                            <option>Pussy</option>
-                            <option>lang mananap</option>
-                        </select>
-                    </div>
-                    <div class="submit">
-                        <button class="button-search hvr-pulse">SEARCH</button>
-                    </div>
-                </form>
-
-            </aside>
-
-        </div>
-
-    </div>
-</div> <!--content-wrap-->
-
-
-
-
-<footer class="clearfix">
-    <div class="copyright_wrap">
-        <center><p>Fur and Paws &copy; 2016 All Rights Reserved <a href="#">Terms of Use</a> and <a href="#">Privacy Policy</a></p></center>
-        <center><div class="login_social_list">
-            <ul>
-                <li><a href="#" class="hvr-wobble-vertical"><img src="img/icon/social/facebook.ico" class="icon-circle"></a></li>
-                <li><a href="#" class="hvr-wobble-vertical"><img src="img/icon/social/twitter.ico" class="icon-circle"></a></li>
-                <li><a href="#" class="hvr-wobble-vertical"><img src="img/icon/social/google_plus.ico" class="icon-circle"></a></li>
-                <li><a href="#" class="hvr-wobble-vertical"><img src="img/icon/social/instagram.ico" class="icon-circle"></a></li>
-
-            </ul>
-        </div>
-        </center>
-    </div>
-</footer><!-- end of footer -->
-
-
-<!-- Bootstrap And Core JavaScript
-  ================================================== -->
-<script type="text/javascript" src="js/jquery.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
+@push('body-bottom-scripts')
 <script type="text/javascript" src="js/custom-elevate.js"></script>
-<script type="text/javascript" src="js/loginmodal.js"></script>
-<script type="text/javascript" src="js/custom-scroll.js"></script>
 <script type="text/javascript" src="js/custom-tooltip.js"></script>
-
-</body>
-</html>
+@endpush
